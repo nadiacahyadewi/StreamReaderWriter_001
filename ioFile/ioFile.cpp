cbcc8f5596b1610c75a12408afae02d52bf24f70
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-21 14:55:23 +0700

    unlimited loop untuk menulis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -13,4 +13,14 @@ int main() {
 
 	cout << ">== Menulis file, \'q\ untuk keluar" << endl;
 
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris 
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai daris 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
