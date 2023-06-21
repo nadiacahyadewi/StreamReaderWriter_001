f749a1e5857e55c5e1342daf7ec625ac7ab1537b
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-21 15:46:10 +0700

    membuka dan membaca file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -31,4 +31,13 @@ int main() {
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//membuka file dalam mode membaca
+	ifstream infile;
+
+	//menunjuk ke sebuah file
+	infile.open(NamaFile, ios::in);
+
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
 	
+}
