e9761275ecc8a15a6f9e7bcd0b3534fc13844ede
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-21 16:01:31 +0700

    penangkap menggunakan obyek exception

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -4,3 +4,19 @@
 #include <array>
 //untuk objek array yang akan kita gunakan
 using namespace std;
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:....
+	try {
+		array<int, 3> data = { 3, 3, 7 };
+		//pesan array interger 3 elemen 
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch(exception & e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl; 
+		/ akan diekseskusi karna array data hanya memiliki 3 elemen / 
+	}
+	
+}
