584f3b7e5afb17d6c0475e7fd632316da1174048
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-21 15:08:29 +0700

    mencari file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -32,5 +32,17 @@ int main() {
 	infile.open("contohfile.txt");
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
+	// jika file ada maka 
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini 
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai 
+		infile.close();
+	}
 	
 }
